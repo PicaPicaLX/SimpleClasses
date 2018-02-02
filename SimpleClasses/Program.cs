edb0c9e5bfaa284cd0d8542b96edc0e21914ec01
栗鑫 <lx389029980@outlook.com>
2018-02-02 23:13:45 +0800

    V1

--- SimpleClasses/Program.cs
+++ SimpleClasses/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/* 提要：通过为一家汽车经销商开发一套能够根据车辆品牌型号生产日期
+ * 进行估值或交易的软件来理解"类(class)"的使用，并创建自定义类来完成学习
+ */
 namespace SimpleClasses
 {
     class Program
