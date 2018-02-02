dca7b78e7ade41eebaff58d104bca577d73a032d
栗鑫 <lx389029980@outlook.com>
2018-02-02 23:11:31 +0800

    Complete!

--- SimpleClasses/Program.cs
+++ SimpleClasses/Program.cs
@@ -10,6 +10,33 @@ namespace SimpleClasses
     {
         static void Main(string[] args)
         {
+            Car myCar1 = new Car();
+            myCar1.Make = "Oldsmobile";
+            myCar1.Model = "Cutlas Supreme";
+            myCar1.Year = 1986;
+            myCar1.Color = "Silver";
+
+            Car myCar2 = new Car();
+            myCar2.Make = "New";
+            myCar2.Model = "Buick";
+            myCar2.Year = 1999;
+            myCar2.Color = "Black";
+
+            Console.WriteLine("{0} {1} {2} {3}", 
+                myCar1.Make, 
+                myCar1.Model, 
+                myCar1.Year, 
+                myCar1.Color);
+            Console.ReadLine();
         }
+    }
+
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
     }
 }
