a19db22f907a11bad906b9282af63f511e701c82
栗鑫 <lx389029980@outlook.com>
2018-02-02 22:25:57 +0800

    A stupid way to store car info.

--- SimpleClasses/Program.cs
+++ SimpleClasses/Program.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/* 提要：通过为一家汽车经销商开发一套能够根据车辆品牌型号生产日期
+ * 进行估值或交易的软件来理解"类(class)"的使用，并创建自定义类来完成学习
+ * 下面先演示一下低效方法，即手动建立一个个数据
+ * 当你想增加车辆属性时，需要手动一个个添加
+ * 不便维护
+ */
 namespace SimpleClasses
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string car1Make;
+            string car1Model;
+            int car1Year;
+            string car1Color;
+
+            string car2Make;
+            string car2Model;
+            int car2Year;
+            string car2Color;
+
+            string car3Make;
+            string car3Model;
+            int car3Year;
+            string car3Color;
+
+            // ......
         }
     }
 }
